190ed01f9d7aea487192b5ed5b673267dfbe5b48
Guillermo Jimenez <jimenezguillermo1208@gmail.com>
2022-12-03 14:23:01 -0400

    funcion _onCreate (no testeada)

--- lib/Infrastructure/Data/sqlite/database_helper.dart
+++ lib/Infrastructure/Data/sqlite/database_helper.dart
@@ -8,17 +8,43 @@ class DatabaseHelper {
   // 1
   static const _databaseName = 'Corsi.db';
   static const _databaseVersion = 1;
-// 2
+  // 2
   static const courseTable = 'Course';
-// 3
+  static const lessonTable = 'Lesson';
+  static const courseId = 'courseId';
+  static const lessonId = 'lessonId';
+  // 3
   static late BriteDatabase _streamDatabase;
-// make this a singleton class
-// 4
+  // make this a singleton class
+  // 4
   DatabaseHelper._privateConstructor();
   static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
-// 5
+  // 5
   static var lock = Lock();
-// only have a single app-wide reference to the database
-// 6
+  // only have a single app-wide reference to the database
+  // 6
   static Database? _database;
+
+  // SQL code to create the database table
+  // 1
+  Future _onCreate(Database db, int version) async {
+    // 2
+    await db.execute('''
+      CREATE TABLE $courseTable (
+        $courseId INTEGER PRIMARY KEY,
+        name TEXT,
+        description TEXT,
+        photo TEXT
+      )
+    ''');
+    //3
+    await db.execute('''
+      CREATE TABLE $lessonTable(
+        $lessonId INTEGER PRIMARY KEY,
+        $courseId INTEGER,
+        name TEXT,
+        description TEXT
+      )
+    ''');
+  }
 }
