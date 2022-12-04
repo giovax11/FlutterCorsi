d8b925a677235b26b79929665247e98c5fc2c5fa
Guillermo Jimenez <jimenezguillermo1208@gmail.com>
2022-12-04 17:04:49 -0400

    cambios

--- lib/Domain/entity/lesson.dart
+++ lib/Domain/entity/lesson.dart
@@ -1,20 +1,25 @@
+import 'package:uuid/uuid.dart';
 
 class Lesson {
   Lesson({
     required this.name,
     required this.description,
+    required id,
   });
 
   String name;
   String description;
+  Uuid id = Uuid();
 
   factory Lesson.fromJson(Map<String, dynamic> json) => Lesson(
-    name: json["name"],
-    description: json["description"],
-  );
+        name: json["name"],
+        description: json["description"],
+        id: json["id"],
+      );
 
   Map<String, dynamic> toJson() => {
-    "name": name,
-    "description": description,
-  };
+        "name": name,
+        "description": description,
+        "id": id,
+      };
 }
