74fe46ff6e973fc922cfc6aedd303c2a0ff0c4bc
giovax11 <giovax1110@gmail.com>
2022-12-04 20:29:29 -0400

    implementacion del Id de lecciones

--- lib/Domain/entity/lesson.dart
+++ lib/Domain/entity/lesson.dart
@@ -1,19 +1,22 @@
 
 class Lesson {
   Lesson({
+    required this.idLesson,
     required this.name,
     required this.description,
   });
-
+  String idLesson;
   String name;
   String description;
 
   factory Lesson.fromJson(Map<String, dynamic> json) => Lesson(
+    idLesson: json["idlesson"],
     name: json["name"],
     description: json["description"],
   );
 
   Map<String, dynamic> toJson() => {
+    "idlesson":idLesson,
     "name": name,
     "description": description,
   };
